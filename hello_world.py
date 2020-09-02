50891d7e442193d0e6363f9f6ea79f3e028836a2
Kari Koivisto <kaiffari1@outlook.com>
2020-09-02 18:19:27 +0300

    modify release

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
 print("Hello world!")
-<<<<<<< HEAD
 
 print("Hello from feature")
 
 print("Hello from release")
-=======
-print("Hello once again")
->>>>>>> remotes/normal/feature/add-logging
+
+# print("Hello once again")
+
